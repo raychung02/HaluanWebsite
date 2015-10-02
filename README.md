42d893f98a4586f639a7a22c9bd75e0244cdfafb
raychung02 <raychung02@gmail.com>
2015-10-02 00:39:32 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Haluan Website
+----------------
+
+by Ray Chung
+
+Website for Haluan Hip-Hop Dance Troupe. Fully responsive and written in HTML5, CSS3, Javascript, and PHP.
+
+About Haluan
+--------------
+
+Haluan is the competitive hip-hop dance team housed under the Filipino Student Association at the University of California, Santa Cruz.
+
+Live URL
+--------
+[raychung02.github.io/haluan](raychung02.github.io/haluan)
+
+Technologies Used
+-----------------
+* HTML5
+* CSS3
+* Javascript
+* PHP
+* jQuery
+* [skel](https://github.com/n33/skel) by n33
+* [Font Awesome](https://fortawesome.github.io/Font-Awesome/)
---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php
+
+	if(isset($_POST['submit'])) {
+	
+		// Submission data.
+		$ipaddress = $_SERVER['REMOTE_ADDR'];
+		$datetime = date('d/m/Y H:i:s');
+
+		// Form data.
+		$to = 'haluan.fsa@gmail.com'; 
+		$name = $_POST['name']; 
+		$email = $_POST['email'];
+		$subject = $_POST['subject'];
+		$message = $_POST['message'];
+		$body = "Contact Form Submission - En Route Website.\r\n
+				Name: {$name}\r\n
+				Email Address: {$email}\r\n
+				Subject: {$subject}\r\n
+				Message: {$message}\r\n
+				This message was sent from the IP Address: {$ipaddress} at {$datetime}";
+
+		$headers = "From: $email" . "\r\n" .
+		"Reply-To: $email" . "\r\n" .
+		"X-Mailer: PHP/" . phpversion();
+
+		mail($to, $subject, $body, $headers);
+		header('location: confirm.html');
+
+	}
+?>
